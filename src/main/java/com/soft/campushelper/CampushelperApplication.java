a7334756079683cfef3afdac2d6f134b331d34dc
geon <kky8586696@naver.com>
2024-11-16 22:19:33 +0900

    initial commit

---
+++ src/main/java/com/soft/campushelper/CampushelperApplication.java
@@ -0,0 +1,13 @@
+package com.soft.campushelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampushelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampushelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soft/campushelper/CampushelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.campushelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
