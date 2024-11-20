aaee5172f4d2e12989624eea1b9cee6b44935238
geon <kky8586696@naver.com>
2024-11-20 17:07:18 +0900

    Feat: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/soft/campushelper/CampushelperApplication.java
+++ src/main/java/com/soft/campushelper/CampushelperApplication.java
@@ -3,9 +3,11 @@ package com.soft.campushelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableJpaAuditing
 public class CampushelperApplication {
 
 	public static void main(String[] args) {
